bab432930ddf8115dfc434565ad2c7a561c2e92f
cmx <cmx7o7@qq.com>
2022-05-07 11:36:06 +0800

    third version

--- src/main/java/com/cmx/git/GitTest.java
+++ src/main/java/com/cmx/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args){
         System.out.println("春风得意");
         System.out.println("春风得意马");
+        System.out.println("春风得意马蹄疾");
     }
 }
