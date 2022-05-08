067cbc52ded98e1a2dbb4924cd6b643892cb840a
woshinidie <59273551+Tahuilaima@users.noreply.github.com>
2022-05-08 13:20:04 +0800

    pull version

--- src/main/java/com/cmx/git/GitTest.java
+++ src/main/java/com/cmx/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("春风得意马蹄疾,一");
         System.out.println("春风得意马蹄疾,一日");
         System.out.println("push-test");
+        System.out.println("pull-test");
     }
 }
